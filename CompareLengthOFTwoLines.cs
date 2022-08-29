7feabead9762c419fafd1ed0489139237b6bbcb4
Anisha0906 <anishadas880@gmail.com>
2022-08-29 22:31:38 +0530

    [Anisha] Comparing length oftwo lines by using compare to method

---
+++ CompareLengthOFTwoLines.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionProblem
+{
+    internal class CompareLengthOFTwoLines
+    {
+        public static void CompareLength()
+        {
+            int x1, x2, y1, y2 = 0;
+            Console.Write("Input an integer value: +x1 +x2 +y1 +y2 ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            int x4, x5, y4, y5 = 0;
+            Console.Write("Input an integer value: +x4 +x5 +y4 +y5 ");
+            x4 = Convert.ToInt32(Console.ReadLine());
+            x5 = Convert.ToInt32(Console.ReadLine());
+            y4 = Convert.ToInt32(Console.ReadLine());
+            y5 = Convert.ToInt32(Console.ReadLine());
+            double length;
+
+            double lenght1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            double lenght2 = Math.Sqrt(Math.Pow(x4 - x5, 2) + Math.Pow(y4 - y5, 2));
+            Console.WriteLine("length =" + lenght1);
+            Console.WriteLine("length =" + lenght2);
+            Console.WriteLine(lenght1.CompareTo(lenght2));
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Line Comparision Computation Program");
-            Console.WriteLine("Choose Option\n 1.Length Of A Line\t 2.Equality of two lines");
+            Console.WriteLine("Choose Option\n 1.Length Of A Line\t 2.Equality of two lines\t 3.CompareLength");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -14,6 +14,9 @@
                     break;
                     case 2:
                     EqualityOfTwoLines.Equality();
+                    break;
+                case 3:
+                    CompareLengthOFTwoLines.CompareLength();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong Choice");
