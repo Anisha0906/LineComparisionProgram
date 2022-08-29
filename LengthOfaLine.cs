aa83658d81688048233564f749d5d0102d9b68ba
Anisha0906 <anishadas880@gmail.com>
2022-08-29 21:27:12 +0530

    [Anisha] Calculate length of a line

---
+++ LengthOfaLine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionProblem
+{
+    internal class LengthOfaLine
+    {
+        public static void CalculateLength()
+        {
+            int x1, x2, y1, y2 = 0;
+            Console.Write("Input an integer value: +x1 +x2 +y1 +y2 ");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            x2 = Convert.ToInt32(Console.ReadLine());
+            y1 = Convert.ToInt32(Console.ReadLine());
+            y2 = Convert.ToInt32(Console.ReadLine());
+            double distance;
+            distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("distance =" + distance);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,6 +5,17 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Line Comparision Computation Program");
+            Console.WriteLine("Choose Option\n 1.Length Of A Line");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    LengthOfaLine.CalculateLength();
+                    break;
+                default:
+                    Console.WriteLine("Entered Wrong Choice");
+                    break;
+            }
         }
     }
 }
