c880bf38dda1aee5f76921e1c03a4749c4b80c69
Anisha0906 <anishadas880@gmail.com>
2022-08-29 21:56:03 +0530

    [Anisha] Check Equality of two lines based on the end points

--- Program.cs
+++ Program.cs
@@ -5,12 +5,15 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Line Comparision Computation Program");
-            Console.WriteLine("Choose Option\n 1.Length Of A Line");
+            Console.WriteLine("Choose Option\n 1.Length Of A Line\t 2.Equality of two lines");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
                     LengthOfaLine.CalculateLength();
+                    break;
+                    case 2:
+                    EqualityOfTwoLines.Equality();
                     break;
                 default:
                     Console.WriteLine("Entered Wrong Choice");
